94ff8cecbf2e6029e8afda25abbe1ac485401cf9
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-07 10:20:45 -0300

    chore: Add new and create actions to ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,22 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
+  def show 
     @article = Article.find(params[:id])
+  end
+
+  def new  
+    @article = Article.new  
+  end
+
+  def create 
+    @article = Article.new(title: "...", body: "...", slug: "...")
+
+    if @article.save 
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 end
