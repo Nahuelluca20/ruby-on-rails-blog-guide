05eaffd65bb39c47c5772bdc29af3416571c083f
Nahuel Luca <nahuelluca20@gmail.com>
2024-05-07 23:49:49 -0300

    chore: Update Gemfile and remove commented code
    feat: Improve styling of new article form
    
    chore: Refactor ArticlesController to use strong parameters
    
    feat: Add validation to Article model
    
    chore: Update application layout and remove unused styles
    
    chore: Update Tailwind configuration
    
    commit message goes here

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,4 +21,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+
+  # Using a private method to encapsulate the permissible parameters
+  # is just a good pattern since you'll be able to reuse the same
+  # permit list between create and update. Also, you can specialize
+  # this method with per-user checking of permissible attributes.
+  # https://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  private
+  def article_params
+    params.require(:article).permit(:title, :slug, :body)
+  end
 end
